bac99e515d8938a53c5a78f4e6bc7bb8609c0635
luvlsco <lucas.alan.vlsco@gmail.com>
2026-06-27 20:30:04 -0300

    fix: improve output conversion error messages

--- src/rltoml/main.rs
+++ src/rltoml/main.rs
@@ -56,7 +56,7 @@ fn convert_single(file: &str, out_path: &std::path::Path, verbose: bool, upperca
 
 	if file_name.ends_with(".gan") {
 		let toml = gan::gan_to_toml(file, verbose).unwrap_or_else(|err| {
-			eprintln!("Failed to convert \"{}\" to TOML, {}", get_file_name(file), gan::format_gan_to_toml_error(&err, file, verbose, uppercase));
+			eprintln!("Failed to convert \"{}\" to \"{}\" (TOML): {}", get_file_name(file), out_path.display(), gan::format_gan_to_toml_error(&err, file, verbose, uppercase));
 			std::process::exit(1);
 		});
 		if let Err(err) = std::fs::write(out_path, toml) {
@@ -65,7 +65,7 @@ fn convert_single(file: &str, out_path: &std::path::Path, verbose: bool, upperca
 		}
 	} else if file_name.ends_with(".gan.toml") {
 		gan::toml_to_gan(file, out_path.to_str().unwrap(), verbose).unwrap_or_else(|err| {
-			eprintln!("Failed to convert \"{}\" to GAN, {}", get_file_name(file), gan::format_toml_to_gan_error(&err, verbose));
+			eprintln!("Failed to convert \"{}\" to \"{}\" (GAN): {}", get_file_name(file), out_path.display(), gan::format_toml_to_gan_error(&err, verbose));
 			std::process::exit(1);
 		});
 	}
